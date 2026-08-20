466a373e637bbb641f80aeaf91453cd8788054e9
Robert Toroitich <robertojnr2802@gmail.com>
2026-08-20 13:27:39 +0300

    Added component update line, updated description and speed for herogeometric component

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import '../styles/App.css'
+import HeroGeometric from '../components/Hero'
 
 function App() {
               
   return (
   <div className='hero-section'>
     <HeroGeometric
-      title1="Robert's"
-      title2="Coffee Shop"
-      description="Enjoy the finest coffee from Kenya's leading coffee shop!"
+      title2="HopOn!"
+      description="View public transport vehicle routes, stops and reserve seats for customers or add stops for routes and manage them for service providers"
       color1="#3B82F6"
       color2="#F0F9FF"
-      speed={1.6}
+      speed={0.6}
     />
   </div>
   )
