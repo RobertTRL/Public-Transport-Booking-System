7c083defa546833e589399e71d76950269624cc6
Robert Toroitich <robertojnr2802@gmail.com>
2026-08-20 12:54:34 +0300

    Imported react-router-dom components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './pages/App.jsx'
-// import react-router and react-router-dom dependecies here
+import { BrowserRouter, Routes, Route , Link, NavLink, Outlet } from 'react-router-dom'
 // shadcn UI library, 
 
 createRoot(document.getElementById('root')).render(
