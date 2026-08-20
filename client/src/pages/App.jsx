c3914550dfa0268f2cde7d05476cee91d8595a2d
Robert Toroitich <robertojnr2802@gmail.com>
2026-08-20 13:19:34 +0300

    Added hero style import line, hero code in App.jsx

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import '../styles/App.css'
+import '../styles/hero.css'
 
 function App() {
-
               
   return (
- <div>
-      <h1>Welcome to the App</h1>
-    </div>
+  <div className='hero-section'>
+    <HeroGeometric
+      title1="Robert's"
+      title2="Coffee Shop"
+      description="Enjoy the finest coffee from Kenya's leading coffee shop!"
+      color1="#3B82F6"
+      color2="#F0F9FF"
+      speed={1.6}
+    />
+  </div>
   )
 }
 
