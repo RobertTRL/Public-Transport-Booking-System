7fb37f3bc64e9bc5719136a8b0bb1956e42a0ace
Robert Toroitich <robertojnr2802@gmail.com>
2026-08-20 19:13:04 +0300

    Resolved import error for non-existent file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,14 +6,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login.jsx'
 import AccountCreation from './pages/AccountCreation.jsx'
 import Dashboard from './pages/Dashboard.jsx'
-import UserComponentsTest from './pages/UserComponentsTest.jsx'
+// import UserComponentsTest from './pages/UserComponentsTest.jsx'
 // shadcn UI library, 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/user-test" element={<UserComponentsTest />} />
+        {/* <Route path="/user-test" element={<UserComponentsTest />} /> */}
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path='/signup' element={<AccountCreation />}/>
