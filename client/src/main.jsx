91a78dee9ecb879dd442f2c451cd4ce05c66a357
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-08-20 20:28:06 +0200

    resolve authentication routing conflict

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,11 +6,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './styles/index.css'
 import App from './pages/App.jsx'
+import Login from './pages/Login.jsx'
+import AccountCreation from './pages/AccountCreation.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+     <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<AccountCreation />} />
+        
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
