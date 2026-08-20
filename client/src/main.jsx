58010ed2e45aa80297802663de4893d599c8601a
stephenmuasya-ke <stephen.muasya@student.moringaschool.com>
2026-08-20 18:48:13 +0300

    added the  user search and profile components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,16 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './pages/App.jsx'
-import { BrowserRouter, Routes, Route , Link, NavLink, Outlet } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login.jsx'
 import AccountCreation from './pages/AccountCreation.jsx'
 import Dashboard from './pages/Dashboard.jsx'
+import UserComponentsTest from './pages/UserComponentsTest.jsx'
 // shadcn UI library, 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/user-test" element={<UserComponentsTest />} />
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path='/signup' element={<AccountCreation />}/>
