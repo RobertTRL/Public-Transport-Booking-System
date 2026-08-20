c28fc88af9218d49197d6cc3b5b0fb8c9316feb7
Robert Toroitich <robertojnr2802@gmail.com>
2026-08-20 13:43:31 +0300

    Feature herosection (#9)
    * Imported react-router-dom components
    
    * Installed react three fiber, three and framer-motion packages
    
    * Created hero component
    
    * Added code in hero component
    
    * Created background.css file
    
    * Added code in background.css file
    
    * Renamed background.css to hero.css
    
    * Changed css file name in import line to hero.css
    
    * Added hero style import line, hero code in App.jsx
    
    * Removed hero style import line in app.jsx
    
    * Added component update line, updated description and speed for herogeometric component
    
    * Updated index.css style
    
    * Added exclamation mark in description
    
    * Moved title style and font download from hero.css to index.css, renamed class name

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import '../styles/App.css'
+import HeroGeometric from '../components/Hero'
 
 function App() {
-
               
   return (
- <div>
-      <h1>Welcome to the App</h1>
-    </div>
+  <div className='hero-section'>
+    <HeroGeometric
+      title2="HopOn!"
+      description="View public transport vehicle routes, stops and reserve seats for customers or add stops for routes and manage them for service providers!"
+      color1="#3B82F6"
+      color2="#F0F9FF"
+      speed={0.6}
+    />
+  </div>
   )
 }
 
