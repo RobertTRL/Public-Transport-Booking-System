bd48e1f6ededcb4341410d2864abc133dd940cf0
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-08-20 16:44:00 +0300

    added the content the footer should return

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,13 @@
 function Footer() {
     const currentYear = new Date().getFullYear();
 
-
-
-
     return(
-
+        <footer className="footer">
+            <div className="footer-content">
+                <p>Copyright &copy; {currentYear} HopOn! All rights reserved.</p>
+            </div>
+        </footer>
     )
 }
+
+export default Footer
