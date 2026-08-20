b405726ae766ed3e34c27a4565df67443554b0ad
Robert Toroitich <robertojnr2802@gmail.com>
2026-08-20 13:33:42 +0300

    Added exclamation mark in description

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -8,7 +8,7 @@ function App() {
   <div className='hero-section'>
     <HeroGeometric
       title2="HopOn!"
-      description="View public transport vehicle routes, stops and reserve seats for customers or add stops for routes and manage them for service providers"
+      description="View public transport vehicle routes, stops and reserve seats for customers or add stops for routes and manage them for service providers!"
       color1="#3B82F6"
       color2="#F0F9FF"
       speed={0.6}
