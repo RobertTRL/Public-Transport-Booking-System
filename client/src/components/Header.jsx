8febce355a1cda4493376d8966224778346d4335
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-08-20 16:55:19 +0300

    added app name to header and exported it

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,9 +2,12 @@ function Header({title}) {
     return(
         <header className="header">
             <div className="header-content">
-                <h1>{title}</h1>
+                <h1>Hop on!</h1>
+                <h2>{title}</h2>
             </div>
         </header>
     )
 }
+
+export default Header
     
