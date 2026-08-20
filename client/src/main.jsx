f25826d4d700ed3c13df4adc5b4074d3caa0c435
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-08-20 20:18:05 +0200

    importing routes dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 import './styles/index.css'
 import App from './pages/App.jsx'
