2e7f2dc385b4afb74d30f3390e5d582c0c94406f
Robert Toroitich <robertojnr2802@gmail.com>
2026-08-21 22:59:33 +0300

    Edited description in problem component

--- client/src/components/Problem.jsx
+++ client/src/components/Problem.jsx
@@ -5,8 +5,8 @@ export default function Problem() {
         <section className="problem">
             <div className="problem__inner">
                 <div className="problem__text">
-                    <h2 className="problem__title">Problem</h2>
-                    <p className="problem__description">Ever wanted to view the route of a public service vehicle with stops? Or have you once tried boarding a vehicle only to be disappointed and infuriated when you see it is full? In such situations, did you ever wish that there was an easier way to reserve a seat, or view a route?</p>
+                    <h2 className="problem__title">Picture this.</h2>
+                    <p className="problem__description">You are in the streets of Nairobi. You want to board a public transport vehicle, with the convenience of a private taxi. However, you wondered, where can I view routes of a public service vehicle with stops? Or have you once tried boarding a vehicle only to be disappointed and infuriated when you see it is full? In such situations, did you ever wish that there was an easier way to reserve a seat, or view a route?</p>
                 </div>
 
                 <div className="problem__image">
