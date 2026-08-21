ebf07d98304c770c74f65ed57f46c3ce6a6617a1
Robert Toroitich <robertojnr2802@gmail.com>
2026-08-21 12:38:28 +0300

    Added index subroute for dashboard route in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import App from "./pages/App.jsx";
 import Login from "./pages/Login.jsx";
 import AccountCreation from "./pages/AccountCreation.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Home from "./pages/Home.jsx";
+import DashboardSummary from "./components/Summary.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -16,7 +16,9 @@ createRoot(document.getElementById("root")).render(
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<AccountCreation />} />
-        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route index element={<DashboardSummary/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
