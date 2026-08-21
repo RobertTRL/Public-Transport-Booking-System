a67e2ec80fe2c130a8b4d2cf6bcc38aa342bb830
Robert Toroitich <robertojnr2802@gmail.com>
2026-08-21 23:48:17 +0300

    Added title, description, changed image src of Solution

--- client/src/components/Solution.jsx
+++ client/src/components/Solution.jsx
@@ -6,12 +6,16 @@ export default function Solution() {
             <div className="problem__inner">
 
                 <div className="problem__image">
-                    <img src="/images/Questions.svg" alt="..." />
+                    <img src="/images/Team goals-cuate.svg" alt="..." />
                 </div>
-                
+
                 <div className="problem__text">
-                    <h2 className="problem__title">Picture this.</h2>
-                    <p className="problem__description">You are in the streets of Nairobi. You want to board a public transport vehicle, with the convenience of a private taxi. However, you wondered, where can I view routes of a public service vehicle with stops? Or have you once tried boarding a vehicle only to be disappointed and infuriated when you see it is full? In such situations, did you ever wish that there was an easier way to reserve a seat, or view a route?</p>
+                    <h2 className="problem__title">Worry no more,</h2>
+                    <p className="problem__description">We, as Hop On! have crafted a solution to your problem.
+                        We have implemented a <strong>Public Transport Booking System</strong> where you 
+                        can <strong>view route maps and stops</strong>, look up and <strong>reserve</strong> seats for available buses 
+                        going through your desired routes. Additionally, service operators can <strong>add stops for routes</strong> and manage the routes they operate in. 
+                    </p>
                 </div>
 
             </div>
