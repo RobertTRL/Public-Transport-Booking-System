310754eea123e4361995fad9e007668f8e2dc9c1
stephenmuasya-ke <stephen.muasya@student.moringaschool.com>
2026-08-21 16:15:39 +0300

    style: improve transport route visibility

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -4,20 +4,41 @@ import {
   Marker,
   Popup,
   Polyline,
+  useMap,
 } from "react-leaflet";
+import { useEffect } from "react";
 import "leaflet/dist/leaflet.css";
 
 const DEFAULT_CENTER = [-1.286389, 36.817223];
 
-function Map({ locations = [], routes = [] }) {
-  const center = locations.length > 0
-    ? locations[0].position
-    : DEFAULT_CENTER;
+function MapBounds({ locations, routes }) {
+  const map = useMap();
 
+  useEffect(() => {
+    const points = [
+      ...locations.map((location) => location.position),
+      ...routes.flatMap((route) => route.positions || []),
+    ];
+
+    if (points.length === 0) {
+      map.setView(DEFAULT_CENTER, 12);
+      return;
+    }
+
+    map.fitBounds(points, {
+      padding: [40, 40],
+      maxZoom: 14,
+    });
+  }, [map, locations, routes]);
+
+  return null;
+}
+
+function Map({ locations = [], routes = [] }) {
   return (
     <div className="transport-map">
       <MapContainer
-        center={center}
+        center={DEFAULT_CENTER}
         zoom={12}
         scrollWheelZoom={false}
         className="map-container"
@@ -27,15 +48,26 @@ function Map({ locations = [], routes = [] }) {
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
 
+        <MapBounds
+          locations={locations}
+          routes={routes}
+        />
+
         {/* Route lines */}
         {routes.map((route) => (
           <Polyline
-            key={route.id}
-            positions={route.positions}
-          >
+             key={route.id}
+              positions={route.positions}
+              pathOptions={{
+               color: route.color || "#2563eb",
+             weight: 5,
+              opacity: 0.8,
+               }}
+            >
             <Popup>
               <div className="map-popup">
                 <h3>{route.name}</h3>
+
                 {route.description && (
                   <p>{route.description}</p>
                 )}
