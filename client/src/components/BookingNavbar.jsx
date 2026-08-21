bad7b9b3faa8e77ef659efa51f74a0ac458cb67a
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-08-21 20:43:23 +0300

    added nav link for home in booking

--- client/src/components/BookingNavbar.jsx
+++ client/src/components/BookingNavbar.jsx
@@ -1,3 +1,13 @@
 import { NavLink } from "react-router-dom";
 import "../styles/bookings.css";
 
+function BookingNavbar(){
+    return(
+    <nav className="booking-navbar">
+      <NavLink to="/booking" end className="nav-btn">
+        <span className="icon">🏠</span>
+        <span>Home</span>
+      </NavLink>
+    </nav>
+    )
+}
