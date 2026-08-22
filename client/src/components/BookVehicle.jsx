401b0d7b6d341ca8623f0bce46c9c212bae43b25
Robert Toroitich <robertojnr2802@gmail.com>
2026-08-22 14:48:04 +0300

    Replaced title, description and image src for bookvehicle component

--- client/src/components/BookVehicle.jsx
+++ client/src/components/BookVehicle.jsx
@@ -6,11 +6,14 @@ export default function BookVehicle() {
             <div className="problem__inner">
                 <div className="problem__text">
                     <h2 className="problem__title">2. Book a vehicle and reserve a seat</h2>
-                    <p className="problem__description">You are in the streets of Nairobi. You want to board a public transport vehicle, with the convenience of a private taxi. However, you wondered, where can I view routes of a public service vehicle with stops? Or have you once tried boarding a vehicle only to be disappointed and infuriated when you see it is full? In such situations, did you ever wish that there was an easier way to reserve a seat, or view a route?</p>
+                    <p className="problem__description">Once you are done selecting your route, 
+                        pick any available vehicle passing going through it. Reserve a seat, and your done!
+                        Just sit tight as you wait for the vehicle's arrival.
+                        </p>
                 </div>
 
                 <div className="problem__image">
-                    <img src="/images/Questions.svg" alt="..." />
+                    <img src="/images/city bus.svg" alt="..." />
                 </div>
             </div>
         </section>
