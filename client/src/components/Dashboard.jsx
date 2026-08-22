fad2cd9664a2ee8160f4882e84926f2597bda51f
Moosealot <normanmondohamaze@gmail.com>
2026-08-22 10:52:03 +0300

    Update dashboard layout for nested pages

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
+import "../styles/dashboard.css";
 
 function Dashboard() {
   return (
