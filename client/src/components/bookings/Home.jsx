05794c994705f8e9cca91eb5cb827a1284d567eb
Robert Toroitich <robertojnr2802@gmail.com>
2026-08-22 18:45:22 +0300

    Added space in hop on label title

--- client/src/components/bookings/Home.jsx
+++ client/src/components/bookings/Home.jsx
@@ -44,7 +44,7 @@ function Home() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, ease: "easeOut" }}
         >
-          HopOn
+          Hop On
         </motion.p>
 
         <motion.h1
