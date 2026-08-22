ea034322a7ac7ff7bf7b746f90a03dfea03f515c
Robert Toroitich <robertojnr2802@gmail.com>
2026-08-22 18:27:00 +0300

    Added code in homepage.jsx, created and added style in homepage.css, resolved css import error in bookingnavbar

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,4 +1,14 @@
-export default function HomePage() {
-    <>
-    </>
+import { Outlet } from "react-router-dom";
+import BookingNavbar from "../components/bookings/BookingNavbar.jsx";
+import "../styles/homepage.css";
+
+export default function Homepage() {
+    return (
+        <div className="booking-homepage">
+            <BookingNavbar />
+            <main className="booking-homepage__content">
+                <Outlet />
+            </main>
+        </div>
+    );
 }
