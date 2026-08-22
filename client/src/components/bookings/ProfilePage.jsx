9a5d0a77b0318ea4b8c460c006dc35b341587a8f
Robert Toroitich <robertojnr2802@gmail.com>
2026-08-22 17:00:19 +0300

    Resolved css import error in profilepage

--- client/src/components/bookings/ProfilePage.jsx
+++ client/src/components/bookings/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import '../styles/user.css'
+import '../../styles/user.css'
 
 function ProfilePage() {
   const navigate = useNavigate()
