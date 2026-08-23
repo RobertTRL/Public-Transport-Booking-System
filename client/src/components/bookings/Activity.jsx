3a1e74709bbfbfee510ac526210925877fae01b8
Robert Toroitich <robertojnr2802@gmail.com>
2026-08-23 11:19:36 +0300

    Added code in activity component

--- client/src/components/bookings/Activity.jsx
+++ client/src/components/bookings/Activity.jsx
@@ -1,4 +1,16 @@
-export default function Activity() {
-    <>
-    </>
-}
+const Activity = () => {
+  return (
+    <div className="activity-empty">
+      <img
+        src="/images/Bus Stop-cuate.svg"
+        alt="No activity"
+        className="activity-empty__image"
+      />
+      <p className="activity-empty__text">
+        No activity, give this space some love
+      </p>
+    </div>
+  );
+};
+
+export default Activity;
