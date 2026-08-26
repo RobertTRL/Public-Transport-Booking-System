dcde9235e66573c48d79e0729863099814252993
Robert Toroitich <robertojnr2802@gmail.com>
2026-08-26 10:56:46 +0300

    Assigned tasks for routes

--- server/app.py
+++ server/app.py
@@ -17,12 +17,14 @@ from models import (
 
 """
 # IAM endpoints for passengers and providers
+# Robert
 POST	            /api/v1/auth/passenger/register	                                                        register_passenger() ✓
 POST	            /api/v1/auth/provider/register	                                                        register_provider() ✓
 POST	            /api/v1/auth/passenger/login	                                                        login_passenger() ✓
 POST	            /api/v1/auth/provider/login	                                                            login_provider() ✓
 
 # Passenger endpoints
+# Norman
 GET	                /api/v1/stops	                                                                        get_stops() ✓
 GET	                /api/v1/routes/search?origin_stop_id=&destination_stop_id=	                            search_routes() ✓
 GET	                /api/v1/trips?origin_routestop_id=&destination_routestop_id=&date=	                    search_available_trips() ✓
@@ -35,6 +37,7 @@ PATCH	            /api/v1/bookings/<int:booking_id>/cancel	                     
 GET	                /api/v1/routes/<int:route_id>	                                                        get_route() 
 
 # Provider endpoints
+# Vincent
 GET	                /api/v1/provider/dashboard	                                                            get_dashboard_summary() ✓
 GET	                /api/v1/provider/routes	                                                                list_provider_routes() ✓
 POST	            /api/v1/provider/routes	                                                                create_route() ✓
@@ -44,6 +47,8 @@ DELETE              /api/v1/provider/routes/<int:route_id>                      
 POST	            /api/v1/provider/routes/<int:route_id>/stops	                                        add_route_stop() ✓
 PUT                 /api/v1/provider/routes/<int:route_id>/stops                                            replace_route_stops() ✓
 GET                 /api/v1/provider/routes/<int:route_id>/stops                                            list_route_stops() ✓
+
+# Marlene
 PATCH	            /api/v1/provider/routes/<int:route_id>/stops/<int:stop_id>	                            update_route_stop() ✓
 DELETE              /api/v1/provider/routes/<int:route_id>/stops/<int:stop_id>	                            remove_route_stop() ✓
 GET	                /api/v1/provider/vehicles	                                                            list_vehicles() ✓
@@ -52,6 +57,8 @@ PATCH               /api/v1/stops/<int:stop_id>	                                
 DELETE              /api/v1/stops/<int:stop_id>	                                                            delete_stop() ✓
 GET                 /api/v1/provider/vehicles?route_id=&q=                                                  list_provider_vehicles() ✓
 POST	            /api/v1/provider/vehicles	                                                            create_vehicle() ✓
+
+# Stephen
 GET                 /api/v1/provider/vehicles/<int:vehicle_id>	                                            get_vehicle() ✓
 PATCH	            /api/v1/provider/vehicles/<int:vehicle_id>	                                            update_vehicle() ✓
 DELETE              /api/v1/provider/vehicles/<int:vehicle_id>	                                            deactivate_vehicle() ✓
