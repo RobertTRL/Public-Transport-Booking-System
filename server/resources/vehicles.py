b7c33d321f4349b8f02e6e9f27e3eb369a05d6b7
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-08-27 08:19:25 +0200

    support route_id and q filters on list_vehicles

--- server/resources/vehicles.py
+++ server/resources/vehicles.py
@@ -2,7 +2,7 @@ from flask import request
 from flask_restful import Resource
 
 from config import db
-from models import Vehicle
+from models import Vehicle, Trip, RouteStop
 from schemas import VehicleSchema
 
 
@@ -12,5 +12,31 @@ vehicles_schema = VehicleSchema(many=True)
 
 class ListVehiclesResource(Resource):
     def get(self):
-        vehicles = Vehicle.query.all()
+        route_id = request.args.get('route_id', type=int)
+        q = request.args.get('q', type=str)
+
+        query = Vehicle.query
+
+        if route_id:
+            origin_stop = db.aliased(RouteStop)
+            destination_stop = db.aliased(RouteStop)
+
+            query = (
+                query
+                .join(Trip, Trip.vehicle_id == Vehicle.id)
+                .join(origin_stop, Trip.origin_routestop_id == origin_stop.id)
+                .join(destination_stop, Trip.destination_routestop_id == destination_stop.id)
+                .filter(
+                    db.or_(
+                        origin_stop.route_id == route_id,
+                        destination_stop.route_id == route_id
+                    )
+                )
+                .distinct()
+            )
+
+        if q:
+            query = query.filter(Vehicle.number_plate.ilike(f"%{q}%"))
+
+        vehicles = query.all()
         return vehicles_schema.dump(vehicles), 200
