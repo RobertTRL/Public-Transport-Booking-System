d741019c956ab6f34c59cd33654fadca8fdbd3da
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-08-27 09:33:34 +0200

    add GET/POST /api/v1/routes and PATCH/DELETE /api/v1/routes/<id>

--- server/app.py
+++ server/app.py
@@ -28,6 +28,39 @@ api.add_resource(
     '/api/v1/provider/routes/<int:route_id>/stops/<int:stop_id>'
 )
 
+from resources.stops import (
+    CreateStopResource,
+    UpdateStopResource,
+    DeleteStopResource
+)
 
+api.add_resource(
+    CreateStopResource,
+    '/api/v1/stops'
+)
+
+api.add_resource(
+    UpdateStopResource,
+    '/api/v1/stops/<int:stop_id>'
+)
+
+api.add_resource(
+    DeleteStopResource,
+    '/api/v1/stops/<int:stop_id>'
+)
+from resources.routes import (
+    ListCreateRouteResource,
+    UpdateDeleteRouteResource
+)
+
+api.add_resource(
+    ListCreateRouteResource,
+    '/api/v1/routes'
+)
+
+api.add_resource(
+    UpdateDeleteRouteResource,
+    '/api/v1/routes/<int:route_id>'
+)
 if __name__ == '__main__':
     app.run(debug=True)
