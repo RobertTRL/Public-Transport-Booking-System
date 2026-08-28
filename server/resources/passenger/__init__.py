36fa5e7fd9cf40dec7f7316f56d9ca49103479e1
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-08-28 11:56:05 +0300

    Imported passenger resources in passenger init

--- server/resources/passenger/__init__.py
+++ server/resources/passenger/__init__.py
@@ -1,1 +1,22 @@
-# Passenger resources package
+try:
+    from resources.passenger import (
+        booking_endpoints,
+        route_endpoints,
+        stops_endpoints,
+        trip_endpoints,
+    )
+except ImportError:
+    try:
+        from passenger import (
+            booking_endpoints,
+            route_endpoints,
+            stops_endpoints,
+            trip_endpoints,
+        )
+    except ImportError:
+        from server.resources.passenger import (
+            booking_endpoints,
+            route_endpoints,
+            stops_endpoints,
+            trip_endpoints,
+        )
