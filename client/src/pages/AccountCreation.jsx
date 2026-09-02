ff43f4cb5c3da2277984b7fded5f6b05bf6216bc
Robert Toroitich <robertojnr2802@gmail.com>
2026-09-02 16:58:21 +0300

    Resolved registering logic

--- client/src/pages/AccountCreation.jsx
+++ client/src/pages/AccountCreation.jsx
@@ -27,9 +27,18 @@ function AccountCreation() {
       url.searchParams.set("email", email);
       url.searchParams.set("password", password);
 
-      const response = await fetch(url.toString(), {
-        method: "GET",
-        headers: { Accept: "application/json" },
+      const response = await fetch("/api/v1/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          user_type: "passenger",
+          name,
+          email,
+          password,
+        }),
       });
 
       if (!response.ok) {
